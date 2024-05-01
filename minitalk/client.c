63012adda96c9b7443a8bf2bf0693a55d342282c
Grigori Verdyan <grverdya@c2r1s1.42yerevan.am>
2024-05-01 18:56:58 +0400

    minitalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,13 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grverdya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 14:46:22 by grverdya          #+#    #+#             */
+/*   Updated: 2024/04/30 14:48:19 by grverdya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
 #include "libft.h"
 #include "ft_printf.h"
-#include <signal.h>
-#include <stdio.h>
-#include <string.h>
 
 void	throw_error(const char *message)
 {
-	perror(message);
-	exit(EXIT_FAILURE);;
+	ft_printf("%s", message);
+	exit(EXIT_FAILURE);
 }
 
 void	send_signal(int pid, unsigned char character)
@@ -27,28 +37,26 @@ void	send_signal(int pid, unsigned char character)
 				throw_error("Error sending signal to server!");
 		}
 		else if (kill(pid, SIGUSR2) < 0)
-				throw_error("Error sending signal to server!");
+			throw_error("Error sending signal to server!");
 		usleep(100);
 	}
 }
-/*
+
 void	handle_server_signal(int signal, siginfo_t *info, void *context)
 {
 	(void)context;
 	(void)info;
-	if (signal != SIGUSR1)
-		throw_error("Message did not reached to the server successfully\n");
-	else
+	if (signal == SIGUSR1)
 		ft_printf("Message reached to the server successfully\n");
 	exit(EXIT_SUCCESS);
 }
-*/
+
 int	main(int argc, char **argv)
 {
-	pid_t		server_pid;
-	const char	*message;
-	size_t		i;
-//	struct sigaction sa;
+	struct sigaction	sa;
+	pid_t				server_pid;
+	const char			*message;
+	size_t				i;
 
 	if (argc != 3)
 	{
@@ -56,17 +64,17 @@ int	main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	server_pid = ft_atoi(argv[1]);
-	if (server_pid < 0)
+	if (server_pid <= 0)
 		throw_error("Error getting valid server PID!");
 	message = argv[2];
 	i = -1;
-//    sa.sa_sigaction= handle_server_signal;
-//    sigemptyset(&sa.sa_mask);
-//    sa.sa_flags = 0;
-//    sigaction(SIGUSR1, &sa, NULL);
+	sa.sa_sigaction = handle_server_signal;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
 	while (message[++i])
 		send_signal(server_pid, message[i]);
 	send_signal(server_pid, message[i]);
-//	pause();
+	pause();
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,9 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grverdya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 14:45:35 by grverdya          #+#    #+#             */
+/*   Updated: 2024/04/30 14:46:13 by grverdya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
 #include "libft.h"
 #include "ft_printf.h"
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+
+void	throw_error(const char *message)
+{
+	ft_printf("%s", message);
+	exit(EXIT_FAILURE);
+}
 
 void	handle_signal(int signal, siginfo_t *info, void *context)
 {
@@ -11,36 +26,38 @@ void	handle_signal(int signal, siginfo_t *info, void *context)
 	static int				bit_index;
 
 	(void)context;
-	(void)info;
 	current_char |= (signal == SIGUSR1);
 	bit_index++;
 	if (bit_index == 8)
 	{
 		if (current_char == '\0')
 		{
-			ft_printf("\n");
-	//		kill(info->si_pid, SIGUSR1);
+			ft_printf("\nServer PID: %d\n", getpid());
+			if (kill(info->si_pid, SIGUSR1) < 0)
+				throw_error("Error sending back signal to client!");
 		}
 		else
 			ft_printf("%c", current_char);
 		bit_index = 0;
 		current_char = 0;
-		//if (signal == SIGUSR1) {
-          //  kill(info->si_pid, SIGUSR1);
-        //}
 	}
 	else
 		current_char <<= 1;
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	struct sigaction	sa;
 
+	if (argc != 1)
+	{
+		ft_printf("Usage: %s ", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	sa.sa_sigaction = &handle_signal;
-	sa.sa_flags = SA_SIGINFO;
+	sa.sa_flags = 0;
 	sigemptyset(&sa.sa_mask);
-	printf("Server PID: %d\n", getpid());
+	ft_printf("Server PID: %d\n", getpid());
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
